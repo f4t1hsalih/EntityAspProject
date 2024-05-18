cf8e7c56632008c0846a20b07f2afd59ff87670c
f4t1hsalih <f4t1hsalih@gmail.com>
2024-05-18 13:51:08 +0300

    Lecture 90 - Remove Metodu İle Kategori Silme

---
+++ EntityAspProject/CategoryDelete.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="CategoryDelete.aspx.cs" Inherits="EntityAspProject.CategoryDelete" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+
+</asp:Content>
---
+++ EntityAspProject/CategoryDelete.aspx.cs
@@ -0,0 +1,18 @@
+﻿using EntityAspProject.Entity;
+using System;
+
+namespace EntityAspProject
+{
+    public partial class CategoryDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(Request.QueryString["Delete"]);
+            UdemyProductTrackingEntities db = new UdemyProductTrackingEntities();
+            var value = db.tbl_category.Find(x);
+            db.tbl_category.Remove(value);
+            db.SaveChanges();
+            Response.Redirect("Categories.aspx");
+        }
+    }
+}
