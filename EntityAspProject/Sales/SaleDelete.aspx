dcc1954628cfb2b931fd8f161aab0c821fbfd752
f4t1hsalih <f4t1hsalih@gmail.com>
2024-05-28 19:05:56 +0300

    Satışlarda Silme İşlemi Kodlandı

---
+++ EntityAspProject/Sales/SaleDelete.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="SaleDelete.aspx.cs" Inherits="EntityAspProject.Sales.SaleDelete" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
---
+++ EntityAspProject/Sales/SaleDelete.aspx.cs
@@ -0,0 +1,18 @@
+﻿using EntityAspProject.Entity;
+using System;
+
+namespace EntityAspProject.Sales
+{
+    public partial class SaleDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UdemyProductTrackingEntities db = new UdemyProductTrackingEntities();
+            int id = int.Parse(Request.QueryString["Delete"]);
+            var value = db.tbl_sale.Find(id);
+            value.sal_status = false;
+            db.SaveChanges();
+            Response.Redirect("/Sales/SaleList.aspx");
+        }
+    }
+}
