47db2606c0243ea0a272c7a4c7c94a99a92b65ef
f4t1hsalih <f4t1hsalih@gmail.com>
2024-05-28 18:23:12 +0300

    Lecture 113 - Yeni Satışlar İçin DropDownların Doldurulması

--- EntityAspProject/Sales/NewSale.aspx.cs
+++ EntityAspProject/Sales/NewSale.aspx.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EntityAspProject.Entity;
+using System;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace EntityAspProject.Sales
 {
     public partial class NewSale : System.Web.UI.Page
     {
+        UdemyProductTrackingEntities db = new UdemyProductTrackingEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                //Ürünler
+                var product = (from x in db.tbl_product where x.prd_status == true select new { x.prd_id, x.prd_name }).ToList();
+                ddlPrdName.DataTextField = "prd_name";
+                ddlPrdName.DataValueField = "prd_id";
+                ddlPrdName.DataSource = product;
+                ddlPrdName.DataBind();
 
+                //Müşteriler
+                var customer = (from x in db.tbl_customer where x.ctm_status == true select new { x.ctm_id, Customer = x.ctm_name + " " + x.ctm_surname }).ToList();
+                ddlCtmName.DataTextField = "customer";
+                ddlCtmName.DataValueField = "ctm_id";
+                ddlCtmName.DataSource = customer;
+                ddlCtmName.DataBind();
+
+                //Çalışan
+                var staff = (from x in db.tbl_staff where x.stf_status == true select new { x.stf_id, x.stf_name_surname }).ToList();
+                ddlStfName.DataTextField = "stf_name_surname";
+                ddlStfName.DataValueField = "stf_id";
+                ddlStfName.DataSource = staff;
+                ddlStfName.DataBind();
+
+            }
         }
     }
 }
