7337213e19eebf1620086593cea3d33c8e9549b9
f4t1hsalih <f4t1hsalih@gmail.com>
2024-05-29 16:54:20 +0300

    Lecture 117 - Linq İstatistikleri Çekme 1

--- EntityAspProject/Statistics/Statistics.aspx.cs
+++ EntityAspProject/Statistics/Statistics.aspx.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EntityAspProject.Entity;
+using System;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace EntityAspProject.Statistics
 {
@@ -11,7 +8,11 @@ namespace EntityAspProject.Statistics
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            UdemyProductTrackingEntities db = new UdemyProductTrackingEntities();
+            Label1.Text = db.tbl_product.Where(x => x.prd_status != false).Count().ToString();
+            Label2.Text = db.tbl_customer.Where(x => x.ctm_status != false).Count().ToString();
+            Label3.Text = db.tbl_sale.Where(x => x.sal_status != false).Sum(x=>x.sal_price).ToString()+" ₺";
+            Label4.Text = db.tbl_category.Where(x => x.ctg_status != false).Count().ToString();
         }
     }
 }
